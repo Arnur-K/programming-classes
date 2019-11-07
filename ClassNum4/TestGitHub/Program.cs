9a52316f0c9b6b06e57a9cbf4659a2660460f7fe
W59513 <W59513@RA117A-20>
2019-11-07 19:54:17 +0100

    a good job

--- ClassNum4/TestGitHub/Program.cs
+++ ClassNum4/TestGitHub/Program.cs
@@ -9,6 +9,7 @@ namespace TestGitHub
             Console.WriteLine("Privet, Arnur");
             Console.WriteLine("Hello, Arnur");
             Console.WriteLine("Salem, Arnur");
+            Console.WriteLine("Extra line from VS");
         }
     }
 }
