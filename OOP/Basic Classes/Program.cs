6a924c31ca0843a8cde3387a779e588b448e4d7a
W59513 <W59513@RA117A-20>
2019-11-28 19:19:26 +0100

    first approach

---
+++ OOP/Basic Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basic_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.age = 20;
+            Console.WriteLine(p1.age);
+        }
+    }
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+    }
+}
