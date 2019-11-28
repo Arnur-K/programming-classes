8fd6e1019e0ddd0c48474d063eae088c044728bd
W59513 <W59513@RA117A-20>
2019-11-28 19:47:24 +0100

    getters

--- OOP/Basic Classes/Program.cs
+++ OOP/Basic Classes/Program.cs
@@ -7,20 +7,24 @@ namespace Basic_Classes
         static void Main(string[] args)
         {
             Person p1 = new Person("John", "Doe", 20);
-
-            Console.WriteLine(p1.age);
+            Console.WriteLine($"{p1.getAge()} {p1.getFirstName()} {p1.getLastName()}");
         }
     }
     class Person
     {
-        public string firstName;
-        public string lastName;
-        public int age;
+         string firstName;
+         string lastName;
+         int age;
         public Person(string firstName, string lastName, int age)
         {
             this.firstName = firstName;
             this.lastName = lastName;
+            if (age < 0)
+                throw new ArgumentOutOfRangeException("Ur face slapped");
             this.age = age;
         }
+        public string getFirstName() => firstName;
+        public string getLastName() => lastName;
+        public int getAge() => age;
     }
 }
