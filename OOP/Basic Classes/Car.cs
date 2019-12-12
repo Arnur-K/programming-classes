efbc076c71944cc338e0df46b235a3505b3f5772
W59513 <W59513@RA117A-19>
2019-12-12 20:14:31 +0100

    car

---
+++ OOP/Basic Classes/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Classes
+{
+    class Car
+    {
+        private string _brand = "";
+        private int _tankCapacity;
+        private double _currentFuelLevel;
+        private int _consumption;
+        private double _totalKm;
+        private double _tripKm;
+
+        public Car(string brand, int tankCapacity, int consumption)
+        {
+            // TODO check if the values are correct and throw argument
+            _brand = brand;
+            _tankCapacity = tankCapacity;
+            _consumption = consumption;
+        }
+
+        public string Brand => _brand;
+        public int TankCapacity => _tankCapacity;
+        public int CurrentFuelLevel => (int)_currentFuelLevel;
+        public int Consumption => _consumption;
+        public int TotalKm => (int)_totalKm;
+        public int TripKm => (int)_tripKm;
+        public void ResetTripKms() => _tripKm = 0;
+
+        public void Tank(int amount)
+        {
+            if (_currentFuelLevel + amount <= _tankCapacity)
+                _currentFuelLevel += amount;
+            else
+                _currentFuelLevel += _tankCapacity;
+        }
+
+        public void Drive(int kms)
+        {
+            var fuelNeedded = kms * _consumption / 100.0;
+            if (_currentFuelLevel >= fuelNeedded) 
+            {
+                _totalKm += kms;
+                _tripKm += kms;
+                _currentFuelLevel -= fuelNeedded;
+            }
+            else 
+            { 
+                var howFarIsPossible = 100.0 * _currentFuelLevel * _consumption;
+                _totalKm += howFarIsPossible;
+                _tripKm += howFarIsPossible;
+                _currentFuelLevel = 0;
+            }
+        }
+    }
+}
--- OOP/Basic Classes/Program.cs
+++ OOP/Basic Classes/Program.cs
@@ -6,18 +6,32 @@ namespace Basic_Classes
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person("John", "Doe", 20);
-            Person p2 = new Person("Ann", "Doe", 25);
+            /*Person p1 = new Person("John", "Zip", 0);
+            Person p2 = new Person("Ann", "Doe", 0);
+            p1.LastName = "Doe";
             Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
             p2.LastName = "Grey";
             Console.WriteLine($"{p2.FirstName} {p2.LastName} {p2.Age}");
+            Console.WriteLine(p1.GetFullName);
+            Console.WriteLine(p1.IsAdult);*/
+
+
+            Car c1 = new Car("Waz", 100, 5);
+            Console.WriteLine($"{c1.Brand}, {c1.TankCapacity}, {c1.Consumption}");
+            c1.Tank(50);
+            Console.WriteLine($"Fuel level is  {c1.CurrentFuelLevel}");
+
+            c1.Tank(14);
+            c1.Drive(20);
+
+            Console.WriteLine($"Fuel level is {c1.CurrentFuelLevel} kms {c1.TotalKm}");
         }
     }
     class Person
     {
-         string firstName;
-         string lastName;
-         int age;
+        string firstName;
+        string lastName;
+        int age;
 
         public string FirstName
         {
@@ -30,16 +44,17 @@ namespace Basic_Classes
             }
         }
 
+        public string GetFullName => $"{firstName } {lastName}";
+
         public string LastName
         {
             get => lastName;
             set => lastName = value;
         }
 
-        public int Age
-        {
-            get => age;
-        }
+        public int Age => age;
+
+        public bool IsAdult => age >= 18;
 
         public Person(string firstName, string lastName, int age)
         {
