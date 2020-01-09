5ffccc5204d464eb161af0e2d7a972cc6ed5ddfb
W59513 <W59513@RA117A-18>
2020-01-09 20:22:30 +0100

    WWS

--- OOP/AdvancedClasses/Program.cs
+++ OOP/AdvancedClasses/Program.cs
@@ -5,9 +5,39 @@ namespace AdvancedClasses
 
     class Program
     {
+
+        public abstract class Pet
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public virtual void MakeSound() { }
+            public Pet(string name, int age) {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        public class Dog : Pet
+        {
+            public Dog(string name, int age) : base(name, age) { }
+            public override void MakeSound()
+            {
+                Console.WriteLine("Bark");
+            }
+        }
+
+        public class Cat : Pet
+        {
+            public Cat(string name, int age) : base(name, age) { }
+            public override void MakeSound()
+            {
+                Console.WriteLine("Wws");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Shape[] ourShapes =
+            /*Shape[] ourShapes =
             {
                 new Triangle(3,4,5),
                 new Square(8),
@@ -19,7 +49,12 @@ namespace AdvancedClasses
             foreach(var shape in ourShapes)
             {
                 Console.WriteLine(shape);
-            }
+            }*/
+
+            Pet tom = new Cat("Tom", 50);
+            tom.MakeSound();
+            tom = new Dog("Kotopes", 60);
+            tom.MakeSound();
         }
     }
 }
