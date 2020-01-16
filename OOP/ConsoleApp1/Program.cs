3200bb8168cc710882aee1c8879fea3d2810be8d
W59513 <W59513@RA117A-18>
2020-01-16 19:11:12 +0100

    Interfaces

---
+++ OOP/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public interface IDrawable
+        {
+            void Draw();
+        }
+
+        public class Vehicle : IDrawable
+        {
+            public void Draw()
+            {
+                Console.WriteLine("Vehicle");
+            }
+        }
+
+        public class  Square : IDrawable
+        {
+            public void Draw()
+            {
+                Console.WriteLine("Square");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IDrawable[] thingsToDraw = {
+                new Square(),
+                new Vehicle()
+            };
+
+            foreach( var i in thingsToDraw) {
+                i.Draw();
+            }
+        }
+    }
+}
