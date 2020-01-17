31cc20298e348a58c4818f239dfeaf77b187c3b2
W59513 <W59513@KM112-19>
2020-01-17 17:47:57 +0100

    File

---
+++ InvoiceApp/InvoiceApp/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace InvoiceApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open file";
+            this.openButton.UseVisualStyleBackColor = true;
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.Location = new System.Drawing.Point(12, 44);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(776, 394);
+            this.resultListBox.TabIndex = 1;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(174, 17);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(37, 13);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "Status";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(93, 12);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Invoice manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ InvoiceApp/InvoiceApp/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InvoiceApp
+{
+    public partial class Form1 : Form
+    {
+        private string _path = @"C:\Users\W59513\Desktop\Invoice1.txt"
+        public Form1()
+        {
+            InitializeComponent();
+            statusLabel.Text = "Press Open file to open file";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var content = File.ReadAllLines(_path);
+        }
+    }
+}
