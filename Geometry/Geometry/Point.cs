17aa8ae5b3f5c1df1e35d76f39b778bc71116176
W59513 <W59513@RA145B-20>
2020-01-30 20:18:32 +0100

    Geometry

---
+++ Geometry/Geometry/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    public interface IMovable
+    {
+        void Move(double x, double y);
+    }
+    public class Point : IMovable
+    {
+        private double _x;
+        private double _y;
+
+        public double X => _x;
+        public double Y => _y;
+
+        public Point()
+        {
+            _x = _y = 0;
+        }
+
+        public Point(double a)
+        {
+            _x = _y = a;
+        }
+
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void Move(double x, double y)
+        {
+            _x += x;
+            _y += y;
+        }
+
+        public virtual double Distance() => Math.Sqrt(_x * _x + _y * _y);
+    }
+}
